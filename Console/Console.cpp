2d8f9a923f924a42e5a484350877812fcbb29ca6
15juga <81560600+15juga@users.noreply.github.com>
2022-11-28 14:32:36 +0100

    Can openfile

--- Console/Console.cpp
+++ Console/Console.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <Reader.h>
+#include "CommonDialogs.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "1. Load File\n2. Exit" << std::endl;
+    int x = 0;
+    std::cin >> x;
 
-    //std::vector<Vertex> mesh;
+    while (x != 2) {
+        
+        if (x == 1) {
+            std::string path = Dialogs::OpenFile("Model (*.acjl)\0*.acjl;\0", "Assets\\");
+            x = 0;
+            if (!path.empty())
+            {
+                LIB::Reader::ReadFile(path.c_str());
+            }
+        }
+        std::cout << "\n\n1. Load File\n2. Exit" << std::endl;
+        std::cin >> x;
+    }
 
-    LIB::Reader::ReadFile("cube.acjl");
-    //LIB::Reader::LoadMesh("boll.acjl", mesh);
+    //LIB::Reader::ReadFile("cube.acjl");
 }
